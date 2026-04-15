23cb75d14848abbd69ef6b462111acc0bc43e288
ramlal-01 <ramlal0801@gmail.com>
2026-04-15 15:12:29 +0530

    deployement of backend

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,3 +1,3 @@
 // frontend/src/config/api.ts
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ?? "http://localhost:5000";
+  import.meta.env.VITE_API_BASE_URL ?? "https://codin.up.railway.app/";
